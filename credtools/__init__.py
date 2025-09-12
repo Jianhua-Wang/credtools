f31b4f64e9b9f134f2ed9637a615d7fe2b5c66e2
Jianhua-Wang <jianhua.mert@gmail.com>
2025-09-12 18:49:11 -0400

    Bump version: 0.0.32 → 0.0.33

--- credtools/__init__.py
+++ credtools/__init__.py
@@ -6,7 +6,7 @@ from rich.logging import RichHandler
 
 __author__ = """Jianhua Wang"""
 __email__ = "jianhua.mert@gmail.com"
-__version__ = "0.0.32"
+__version__ = "0.0.33"
 
 
 logging.basicConfig(
